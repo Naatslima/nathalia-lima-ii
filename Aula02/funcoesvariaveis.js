c4552c443e229d93ab41f5c713265f170dcafe3d
Naatslima <nathaliaxlimma1@gmail.com>
2023-08-21 22:29:42 -0300

    Introdução ao Json

---
+++ Aula02/funcoesvariaveis.js
@@ -0,0 +1,19 @@
+const jovem_mistico = {
+    nome: "Nathalia",
+    signo: "geminiana",
+    idade: 29,
+    louca: true,
+    parentes: ["Xavier", "Lima"]
+
+}
+
+// formato json - javascript notation object
+console.log(jovem_mistico.nome, jovem_mistico.signo)
+console.log(jovem_mistico)
+console.log(typeof jovem_mistico)
+console.log(jovem_mistico.parentes[1])
+
+let pessoas = ["Nat", 29, "Desenvolvedora JR"]
+console.log(pessoas[0], pessoas[2])
+console.log(pessoas)
+console.log(typeof pessoas)
