846d919c225c830991c8b3740c66b554a986f10e
Naats.lima <nathaliaxlimma1@gmail.com>
2023-09-11 22:32:38 -0300

    Primeira modificação no app, prpjeto contador

--- Aula01/meu-primeiro-app/App.js
+++ Aula01/meu-primeiro-app/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from "react-native";
+import Contador from "./components/Contador";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Oii!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Contador/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
